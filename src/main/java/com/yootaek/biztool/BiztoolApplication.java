686776cca3d18efde144ba5e480be1bf58448c56
yootaek <eldbxor@naver.com>
2021-12-23 22:31:49 +0900

    First init

---
+++ src/main/java/com/yootaek/biztool/BiztoolApplication.java
@@ -0,0 +1,13 @@
+package com.yootaek.biztool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiztoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiztoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yootaek/biztool/BiztoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yootaek.biztool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiztoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
